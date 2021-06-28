7c6e6511c3d709c99a4d546ee64bfb12677d1314
solomon-prosperity <solomonprosperityera@gmail.com>
2021-06-28 21:22:53 +0800

    week4

--- serverRequest.js
+++ serverRequest.js
@@ -1,10 +1,13 @@
-const fetch = require('node-fetch');
-
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(json => {
-        for (let i = 0 ; i < json.length ; i++) {
-            console.log(json[i].name)
+const https = require('https');
+let data = "";
+https.get("https://jsonplaceholder.typicode.com/users",(res)=>{
+    res.on('data',(chunk) => {
+        data += chunk;
+    });
+    res.on('end', () => {
+         data = JSON.parse(data)
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].name);   
         }
     })
-    .catch (err =>  console.error(err) )
+})
